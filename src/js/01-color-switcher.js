f4f97bb7e1425bb90d08152d2fe41ba67f0ba1c7
owin66 <78900270+owin66@users.noreply.github.com>
2023-01-16 23:19:42 +0200

    finish_1_ task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+let colorSwitcher = '';
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.start.addEventListener('click', handleStart);
+refs.stop.addEventListener('click', handleStop);
+
+function handleStart(e) {
+  refs.start.setAttribute('disabled', true);
+  refs.stop.removeAttribute('disabled');
+
+  colorSwitcher = setInterval(() => {
+    console.log(refs.body.style.backgroundColor);
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function handleStop(e) {
+  clearInterval(colorSwitcher);
+  refs.stop.setAttribute('disabled', true);
+  refs.start.removeAttribute('disabled');
+}
