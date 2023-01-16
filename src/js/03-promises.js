f062a1a52f1fc13a1867795299c349b5aef0fe69
owin66 <78900270+owin66@users.noreply.github.com>
2023-01-17 00:40:09 +0200

    close_task_3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const inputForm = document.querySelector('form');
+
+function onInputGetValue(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const step = Number(formElements.step.value);
+  const amount = Number(formElements.amount.value);
+  let delay = Number(formElements.delay.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    if (i > 1) {
+      delay += step;
+    }
+    createPromise(i, delay)
+      .then(result => {
+        Notify.success(result);
+      })
+      .catch(error => {
+        Notify.failure(error);
+      });
   }
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+}
+
+inputForm.addEventListener('submit', onInputGetValue);
