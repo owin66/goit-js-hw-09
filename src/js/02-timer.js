efb5bc64c6c2b5a07b11b78b3ffec902ec301597
owin66 <78900270+owin66@users.noreply.github.com>
2023-01-17 00:15:19 +0200

    close_2_task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,77 @@
-// Описан в документации
 import flatpickr from 'flatpickr';
-// Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
-import { Report } from 'notiflix/build/notiflix-report-aio';
+import Notiflix from 'notiflix';
+
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  start: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
+
+refs.start.setAttribute('disabled', '');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < options.defaultDate) {
+      Notiflix.Notify.warning('Please choose a date in the future');
+      return;
+    }
+    refs.start.removeAttribute('disabled', '');
+    options.defaultDate = selectedDates[0].getTime();
+  },
+};
+
+flatpickr(refs.input, options);
+
+const timer = {
+  intervalId: null,
+  start() {
+    this.intervalId = setInterval(() => {
+      const startTime = options.defaultDate;
+      const currentTime = Date.now();
+      const deltaTime = startTime - currentTime;
+      if (deltaTime < 0) {
+        clearInterval(this.intervalId);
+        return;
+      }
+      const time = convertMs(deltaTime);
+      updateClock(time);
+    }, 1000);
+  },
+};
+
+refs.start.addEventListener('click', timer.start);
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateClock({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
+}
